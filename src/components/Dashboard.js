a488cfaaf97113edfb5ac0c2946aabe28fe75fb8
Mikey <michaelp.esteban@gmail.com>
2021-02-12 13:17:13 -0500

    added users list

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,4 +1,5 @@
-import React, { useEffect, Fragment } from 'react'
+import React, { useState, useEffect, Fragment } from 'react'
+import axios from 'axios'
 import styled from 'styled-components'
 import Navbar from './Navbar'
 
@@ -13,18 +14,40 @@ const StatusWrapper = styled.div`
 const Dashboard = (props) => {
   console.log(props)
 
+  const [ users, setUsers ] = useState([])
+
   useEffect( () => {
     const timer = setTimeout(() => {
       document.querySelector('.timedMessage').style.display = 'none';
     }, 5000)
+
+    axios.get('http://localhost:3000/api/v1/users', {
+      headers: {
+        'Authorization': localStorage.getItem('token')
+      }
+    })
+      .then( resp => {
+        debugger
+        setUsers(resp.data.data)
+      })
+      .catch( resp => {
+        debugger
+        console.log(resp)
+      })
+
     return () => clearTimeout(timer)
   }, [])
+
+  const usersList = users.map( item => {
+    return( <li>{item.attributes.name}: {item.attributes.email}</li>)
+  })
 
   return (
     <Fragment>
       <StatusWrapper className="timedMessage">{props.location.state.statusMessage.text}</StatusWrapper>
       <Navbar user={props.location.state.user} />
       <div>[This is my Dashboard component]</div>
+      <ul>{usersList}</ul>
     </Fragment>
   )
 }
