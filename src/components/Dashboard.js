62b2509385c3c8a5876bcea148c22905c62476fb
Mikey <michaelp.esteban@gmail.com>
2021-02-15 20:34:55 -0500

    deleted comments

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -3,10 +3,6 @@ import axios from 'axios'
 import styled from 'styled-components'
 import Navbar from './Navbar'
 import FriendRequests from './FriendRequests'
-// import Button from './UI/buttons/Button'
-// import LightBlueButton from './UI/buttons/LightBlueButton'
-// import OrangeButton from './UI/buttons/OrangeButton'
-// import GrayButton from './UI/buttons/GrayButton'
 
 // headers for axios authorization
 const headers = {
@@ -31,12 +27,6 @@ const StatusWrapper = styled.div`
   font-weight: 600;
   color: #0088cc; /* blue */
 `
-
-// const List = styled.ul`
-//   li {
-//     margin: 10px 0;
-//   }
-// `
 
 const Dashboard = (props) => {
 
@@ -109,77 +99,6 @@ const Dashboard = (props) => {
       .catch( resp => console.log(resp))
   }
 
-  // const checkFriendRequestStatus = (user_id) => {
-  //   const sentStatusObj = sentFriendRequests.filter(item => {
-  //     return item.attributes.receiver_id.toString() === user_id
-  //   })
-  //
-  //   const receivedStatusObj = receivedFriendRequests.filter( item => {
-  //     return item.attributes.requestor_id.toString() === user_id
-  //   })
-  //
-  //   let status = null
-  //   let user = null
-  //   if (sentStatusObj.length > 0) {
-  //     status = sentStatusObj[0]['attributes']['status']
-  //     return {status: status, user: 'sender', friendRequest: sentStatusObj[0]}
-  //   } else if (receivedStatusObj.length > 0) {
-  //     status = receivedStatusObj[0]['attributes']['status']
-  //     return {status: status, user: 'receiver', friendRequest: receivedStatusObj[0]}
-  //   } else {
-  //     return {status, user}
-  //   }
-  // }
-
-  // const friendRequestButton = (user_id) => {
-  //   const {status, user, friendRequest} = checkFriendRequestStatus(user_id)
-  //   if ( status === 'pending' && user === 'sender' ) {
-  //     return <LightBlueButton> sent! </LightBlueButton>
-  //   } else if (status === 'pending' && user === 'receiver' ) {
-  //     return (
-  //       <Fragment>
-  //         <LightBlueButton onClick={() => handleAcceptFriendRequest(friendRequest)}>
-  //           accept
-  //         </LightBlueButton>
-  //         <Button>reject</Button>
-  //       </Fragment>
-  //     )
-  //   }  else if ( status === null || status === 'rejected' ) {
-  //     return (
-  //       <OrangeButton onClick={() => handleSendFriendRequest(user_id)}>
-  //         send request
-  //       </OrangeButton>
-  //     )
-  //   } else if ( status === 'accepted' ) {
-  //     return <GrayButton>friend</GrayButton>
-  //   }
-  // }
-
-  // const usersList = users.map( item => {
-  //   return(
-  //     <li key={item.id}>
-  //       {item.attributes.name}
-  //       { friendRequestButton(item.id) }
-  //     </li>
-  //   )
-  // })
-
-  // const sentFriendRequestsList = sentFriendRequests.map( item => {
-  //   return (
-  //     <li key={item.id}>
-  //       sender: {item.attributes.requestor_id}, receiver: {item.attributes.receiver_id}, status: {item.attributes.status}
-  //     </li>
-  //   )
-  // })
-  //
-  // const receivedFriendRequestsList = receivedFriendRequests.map( item => {
-  //   return (
-  //     <li key={item.id}>
-  //       sender: {item.attributes.requestor_id}, receiver: {item.attributes.receiver_id}, status: {item.attributes.status}
-  //     </li>
-  //   )
-  // })
-
   return (
     <Fragment>
       <StatusWrapper className="timedMessage">{props.location.state.statusMessage.text}</StatusWrapper>
@@ -192,19 +111,6 @@ const Dashboard = (props) => {
           handleAcceptFriendRequest={handleAcceptFriendRequest}
           receivedFriendRequests={receivedFriendRequests}
         />
-        {/* <div>[This is my Dashboard component]</div>
-        <div className="users">
-          <p>People to friend...</p>
-          <List>{usersList}</List>
-        </div>
-        <div className="sent-friend-requests-status">
-          <p>Sent requests</p>
-          <List>{sentFriendRequestsList}</List>
-        </div>
-        <div className="received-friend-requests-status">
-          <p>Respond back!</p>
-          <List>{receivedFriendRequestsList}</List>
-        </div> */}
       </DashboardWrapper>
     </Fragment>
   )
