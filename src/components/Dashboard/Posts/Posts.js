9e4ff5f13938bbd524d8484b4f50cdb3c726f2d1
Mikey <michaelp.esteban@gmail.com>
2021-02-22 12:13:54 -0500

    moved toggleLike component into Post component

--- src/components/Dashboard/Posts/Posts.js
+++ src/components/Dashboard/Posts/Posts.js
@@ -1,6 +1,4 @@
-import React, { useContext } from 'react'
-import axiosApiInstance from '../../interceptor'
-import { UserContext } from '../Dashboard'
+import React from 'react'
 import styled from 'styled-components'
 import Post from './Post'
 
@@ -12,30 +10,8 @@ const CardWrapper = styled.div`
 
 const Posts = (props) => {
 
-  const { currentUser } = useContext(UserContext)
   const posts = props.posts
-
-  const toggleLike = (post, hasUserLiked) => {
-    if (!hasUserLiked) {
-      // Api call to like user post
-      axiosApiInstance.post(`http://localhost:3000/api/v1/posts/${post.id}/likes`, { post: post})
-        .then( resp => {
-          debugger
-        })
-        .catch( resp => console.log(resp))
-    } else {
-      // find like
-      const like = post.attributes.likes.filter(item => item.user_id === currentUser.id)[0]
-      // Api call to unlike user post
-      axiosApiInstance.delete(`http://localhost:3000/api/v1/posts/${post.id}/likes/${like.id}`, { like: like})
-        .then( resp => {
-          debugger
-        })
-        .catch( resp => console.log(resp))
-    }
-  }
-
-  const postsList = posts.map( item => <Post key={item.id} post={item} toggleLike={toggleLike} />)
+  const postsList = posts.map( item => <Post key={item.id} post={item}  />)
 
   return (
     <CardWrapper>
